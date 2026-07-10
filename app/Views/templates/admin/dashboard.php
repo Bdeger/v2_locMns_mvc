b5cd676c7653e11c9a1fdc1dbcbac4e933df49c3
Bdeger <degerbanu2@gmail.com>
2026-07-10 16:14:22 +0200

    dashboard : partie contenu main

--- app/Views/templates/admin/dashboard.php
+++ app/Views/templates/admin/dashboard.php
@@ -1,9 +1,23 @@
 <!-- Views/templates/admin/dashboard.php -->
 <section class="header container">
     <div class="header-top">
-        <img src="/public/image/logo.png">
+        <img src="/public/image/logo2.png">
     </div>
     <div class="header-bottom">
-        <h2>Admin</h2>
+        <h1>Dashboard Admin</h1>
     </div>
 </section>
+<section class="resum container">
+    <h2>Vue d'ensemble</h2>
+    <div class = "card-top">
+        <div>Total Matériel 24</div>
+        <div>Disponible</div>
+    </div>
+    <div class = "card-bottom">
+        <div>En Cours</div>
+        <div>En attente</div>
+    </div>
+
+</section>
+<section class="last-emprunt container"></section>
+<section class="navbar"></section>
