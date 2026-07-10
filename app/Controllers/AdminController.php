53d54b1910609cbab5e71753dc1240dfbfdd8f6c
Bdeger <degerbanu2@gmail.com>
2026-07-10 11:47:06 +0200

    Admin dashboard : AdminController, templates/admin/dashboard.php

---
+++ app/Controllers/AdminController.php
@@ -0,0 +1,22 @@
+<?php 
+// app/Controllers/AdminController.php 
+
+// controleur pour Dahsboard
+
+require_once __DIR__ . "/../Views/View.php";
+require_once __DIR__ . "/Controller.php";
+
+class AdminController extends Controller{
+    public function dashboard():void{
+        // afficher la page d'accueil dashboard
+        $this -> view -> render('admin/dashboard',[
+            'title' => 'Dashboard Admin',
+            'dashboard' => true //active le css du dashboard 
+        ]);
+    }
+}
+
+
+
+
+?>
