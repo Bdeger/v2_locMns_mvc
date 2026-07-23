269eb1d1e5038cf93ac35de86be187110aa3ff73
Bdeger <degerbanu2@gmail.com>
2026-07-23 09:57:48 +0200

    feat: méthode getAllUsers dans UserManager

--- app/Models/UserManager.php
+++ app/Models/UserManager.php
@@ -13,6 +13,16 @@ class UserManager extends Manager{
         $stmt -> execute([$email]);
         return $stmt -> fetch(); //fetch : retourne une seule ligne
     }
+
+    public function getAllUsers():array{
+        $stmt = $this ->pdo -> prepare("
+        SELECT u.nom, u.prenom, u.adresse_postale, u.telephone, u.email,u.date_inscription, u.actif, u.date_derniere_connexion, u.id_utilisateur, 
+            r.nom AS role
+        FROM utilisateur u
+        INNER JOIN role r ON u.id_role = r.id_role");
+        $stmt -> execute();
+        return $stmt -> fetchAll();
+}
 }
 
 
