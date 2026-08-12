9a1502dc0f1b582b336a7b12cefc1ff3af4061cf
Bdeger <degerbanu2@gmail.com>
2026-08-12 23:26:41 +0200

    feat: ajout de la barre de recherche avec bandeau degrade

--- app/Views/templates/user/accueil.php
+++ app/Views/templates/user/accueil.php
@@ -1,1 +1,19 @@
 <?php include __DIR__ . "/navbar.php";?>
+
+<section class="intro">
+    <div class="intro-titre">
+        <h2>Empruntez le matériel dont vous avez besoin</h2>
+    </div>
+
+    <form class="intro-recherche">
+        <input type="text" name="recherche" id="recherche" class="intro-recherche-champ" placeholder="Ordinateur, tablette ...">
+        <input type="submit" value="Rechercher" class="intro-recherche-bouton">
+    </form>
+</section>
+
+<section class="tri-categorie">
+
+</section>
+<section class="list-materiel">
+    
+</section>
